30f93d524665730586b30083be3761274c186ce6
illusion0001 <37698908+illusion0001@users.noreply.github.com>
2022-03-28 11:01:45 -0500

    Update gencl.sh

--- gencl.sh
+++ gencl.sh
@@ -1,11 +1,11 @@
 test+=$'\n<details>'
-test+=$'\n<summary>Changelist</summary>'
+test+=$'\n<summary>Changelog (Click to Expand)</summary>'
 test+=$'\n\n'
 test+=$'```'
 test+=$'\n'$(git log --oneline)
 test+=$'\n```'
 test+=$'\n\n'
 test+='</details>'
-echo "$test" > test4.md
+echo "$test" > cl.md
 echo test
-cat test4.md
+cat cl.md
