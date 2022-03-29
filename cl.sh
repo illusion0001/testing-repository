0895433db4d37e343bcbef19107cadeb729e1e52
illusion0001 <37698908+illusion0001@users.noreply.github.com>
2022-03-29 14:44:13 -0500

    Update cl.sh

--- cl.sh
+++ cl.sh
@@ -1,9 +1,19 @@
-test=$'<details>'
-test+=$'\n<summary>Changelog (Click to Expand)</summary>'
+curl -fLO https://raw.githubusercontent.com/illusion0001/illusion0001.github.io/main/_posts/_supporters.md
+test=$'<details>\n'
+test+=$'<summary>Changelist (Click to Expand)</summary>\n\n'
+test+=$'```\n'
+test+=$''$(git log --oneline | tac | nl | tac)''
+test+=$'\n'
+test+=$'```\n\n'
+test+=$'</details>\n\n'
+test+=$'<details>\n'
+test+=$'<summary>Supporters (Click to Expand)</summary>\n'
+test+=$'\n'
+test+=$''$(cat _supporters.md)''
 test+=$'\n\n'
-test+=$'```'
-test+=$'\n'$(git log --oneline | tac | nl | tac)
-test+=$'\n```'
-test+=$'\n\n'
-test+='</details>'
-echo "$test" > cl.md
+test+=$'</details>\n'
+test+=$'# Patch Files\n'
+test+=$'https://illusion0001.github.io/_patch/patch.zip\n\n'
+echo "$test" >> changelist.md
+cat README.md >> changelist.md
+cat changelist.md
